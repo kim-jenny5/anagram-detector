9dd7994fc4449f17b28620b39d1925b5cbf8ae12
Jenny Kim <kimj1905@gmail.com>
2021-03-12 15:16:36 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_accessor :word
+    
+    def initialize(word)
+        @word = word
+    end
+
+    def match(arr)
+        arr.select do |element|
+            word_sort_arr = @word.split("").sort
+            element_sort_arr = element.split("").sort
+
+            if word_sort_arr == element_sort_arr
+                element.split
+            end
+        end
+    end
+
+    # leggoo = Anagram.new("leggoo")
+    # arr = ["enlists", "google", "inlets", "banana"]
+
+    # binding.pry
+end
